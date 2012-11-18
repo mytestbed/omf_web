a36274a8d8837aa5922991cbad0d01d23fdc90ad
Max Ott <max.ott@nicta.com.au>
2012-11-18 13:15:31 +1100

    Cleaned up the GEC 15 demo. Added a --local-testing flag which allows testing the visualisation using the database in the example directory.

--- example/openflow-gec15/ov_viz_server.rb
+++ example/openflow-gec15/ov_viz_server.rb
@@ -1,9 +1,12 @@
 
 require 'omf_common/lobject'
-OMF::Common::Loggable.init_log 'simple'
+OMF::Common::Loggable.init_log 'of-demo'
 
 
 require 'omf_oml/table'
+
+$testing_database = 'sqlite://example/openflow-gec15/openflow-demo.sq3'
+$oml_database = 'postgres://norbit.npc.nicta.com.au/openflow-demo?user=oml2&password=omlisgoodforyou'
 
 def load_environment
   require 'omf-web/content/file_repository'
@@ -24,19 +27,13 @@ def load_environment
     end
   end
    
+  # Start database adapter
   EM.next_tick do
     EM::run do
-#    Fiber.new do
-      puts "FIBBBB"
-      wv = ExpDB.new(:adapter=>'sqlite', :database=>'example/openflow-gec15/openflow-demo.sq3')
-      #wv = ExpDB.new(:adapter=>'postgres', :host=>'norbit.npc.nicta.com.au', :user=>'oml2', :password=>'omlisgoodforyou', :database=>'openflow-demo')
+      wv = ExpDB.new($oml_database)
       wv.run
-      puts "FIBBBB!!"      
-#    end
     end
   end
-  puts "BOO!!!!"  
-
 end
 
 
@@ -48,6 +45,18 @@ opts = {
   :handlers => {
     # delay connecting to databases to AFTER we may run as daemon
     :pre_rackup => lambda { load_environment },
+    :pre_parse => lambda do |p|
+      p.separator ""
+      p.separator "DEMO options:"
+      p.on("--local-testing", "If set, use local database for testing [#{$testing_database}]") do
+        $oml_database = $testing_database
+      end
+      p.on("--oml-database DATABASE", "Database containing measurement data [#{$oml_database}]") do |f|
+        $oml_database = f
+      end
+      p.separator ""
+    end
+    
   }
 }
 require 'omf_web'
