04354bf9a9e386accb661fbd40ace2dab814580f
Max Ott <max.ott@nicta.com.au>
2013-03-04 22:52:03 +1100

    Added a 'omf_web_demo' bin to make it easier to start the demo site.

--- example/demo/demo_viz_server.rb
+++ example/demo/demo_viz_server.rb
@@ -1,41 +1,57 @@
-
+ENV['BUNDLE_GEMFILE'] = "#{File.dirname(__FILE__)}/../../Gemfile"
+require 'bundler/setup'
 require 'omf_common/lobject'
 OMF::Common::Loggable.init_log 'demo', :searchPath => File.dirname(__FILE__)
 
 
 require 'omf_oml/table'
+require 'omf_web'
 
-def load_environment
-  require 'omf-web/content/file_repository'
-  OMF::Web::FileContentRepository.register_file_repo(:demo, File.join(File.dirname(__FILE__), 'repository'), true)
+class OmfWebDemo
   
-
-  Dir.glob("#{File.dirname(__FILE__)}/data_sources/*.rb").each do |fn|
-    load fn
+  # Configure the web server
+  #
+  OPTS = {
+    :app_name => 'demo',
+    :page_title => 'Vizualisation Demo',
+    :static_dirs_pre => ["#{File.dirname(__FILE__)}/htdocs"],
+    :handlers => {
+      # delay connecting to databases to AFTER we may run as daemon
+      :pre_rackup => lambda { OmfWebDemo.load_environment },
+    }
+  }
+  
+  def self.start(opts = OPTS)
+    #self.load_environemnt()
+    OMF::Web.start(opts)
   end
+      
+  def self.load_environment
+    require 'omf-web/content/file_repository'
+    OMF::Web::FileContentRepository.register_file_repo(:demo, File.join(File.dirname(__FILE__), 'repository'), true)
+    
   
-  require 'yaml'
-  Dir.glob("#{File.dirname(__FILE__)}/widgets/*.yaml").each do |fn|
-    h = YAML.load_file(fn)
-    if w = h['widget']
-      OMF::Web.register_widget w
-    else
-      MObject.error "Don't know what to do with '#{fn}'"
+    Dir.glob("#{File.dirname(__FILE__)}/data_sources/*.rb").each do |fn|
+      load fn
+    end
+    
+    require 'yaml'
+    Dir.glob("#{File.dirname(__FILE__)}/widgets/*.yaml").each do |fn|
+      h = YAML.load_file(fn)
+      if w = h['widget']
+        OMF::Web.register_widget w
+      else
+        MObject.error "Don't know what to do with '#{fn}'"
+      end
     end
   end
 end
 
-
-# Configure the web server
-#
-opts = {
-  :app_name => 'demo',
-  :page_title => 'Vizualisation Demo',
-  :static_dirs_pre => ["#{File.dirname(__FILE__)}/htdocs"],
-  :handlers => {
-    # delay connecting to databases to AFTER we may run as daemon
-    :pre_rackup => lambda { load_environment },
-  }
-}
-require 'omf_web'
-OMF::Web.start(opts)
+if __FILE__ == $0
+  OmfWebDemo.start
+end
+  
+  
+  
+  
+  
