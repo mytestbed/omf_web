2a76fba61167c03b7b0c5fa0a143adc48202d5f0
Max Ott <max.ott@nicta.com.au>
2013-05-31 12:46:02 +1000

    Fixing moustaches

--- share/htdocs/js/mustache.js
+++ share/htdocs/js/mustache.js
@@ -8,21 +8,27 @@ L.provide('OML.mustache', ["vendor/mustache/mustache.js", 'vendor/bootstrap/css/
       version: "0.1",
     }
     
-    var template_url = opts.template;
-    if (! template_url) {
-      throw "Missing template declaration in mustache widget";
-    }
-    var context = {};
+    var context = opts.context || {};
     var el = $('#' + opts.base_id);
-    
-    $.ajax({
-      url: '/resource/' + template_url,
-      type: 'get'
-    }).done(function(template) {
+    var render_f = function(template) {
       var html = Mustache.to_html(template, context);
       el.html(html);
       var i = 0;
-    }) 
+    };
+    
+    var text = opts.text;
+    if (text) { //} != undefined) {
+      render_f(text);
+    } else {
+      var template_url = opts.template;
+      if (! template_url) {
+        throw "Missing template declaration in mustache widget";
+      }
+      $.ajax({
+        url: '/resource/' + template_url,
+        type: 'get'
+      }).done(render_f);
+    }
 
     return moustache;
   }
