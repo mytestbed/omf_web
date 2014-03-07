4b07e38c1c313940feed6f9d93d75b2f51b53c48
Max Ott <max.ott@nicta.com.au>
2014-03-07 14:39:20 +1100

    Primarily whitespace cleanup. Seem to have not been through here in a long time.

--- lib/omf-web/widget/text/maruku/helpers.rb
+++ lib/omf-web/widget/text/maruku/helpers.rb
@@ -30,164 +30,164 @@
 module MaRuKu
 module Helpers
 
-	# if the first is a md_ial, it is used as such
-	def md_el(node_type, children=[], meta={}, al=nil)
-		if  (e=children.first).kind_of?(MDElement) and
-			e.node_type == :ial then
-			if al
-				al += e.ial
-			else
-				al = e.ial
-			end
-			children.shift
-		end
-		e = MDElement.new(node_type, children, meta, al)
-		e.doc = @doc
-		return e
-	end
+  # if the first is a md_ial, it is used as such
+  def md_el(node_type, children=[], meta={}, al=nil)
+    if  (e=children.first).kind_of?(MDElement) and
+      e.node_type == :ial then
+      if al
+        al += e.ial
+      else
+        al = e.ial
+      end
+      children.shift
+    end
+    e = MDElement.new(node_type, children, meta, al)
+    e.doc = @doc
+    return e
+  end
 
-	def md_header(level, children, al=nil, line_no = -1)
-		md_el(:header, children, {:level => level, :line_no => line_no}, al)
-	end
+  def md_header(level, children, al=nil, line_no = -1)
+    md_el(:header, children, {:level => level, :line_no => line_no}, al)
+  end
 
-	# Inline code
-	def md_code(code, al=nil)
-		md_el(:inline_code, [], {:raw_code => code}, al)
-	end
+  # Inline code
+  def md_code(code, al=nil)
+    md_el(:inline_code, [], {:raw_code => code}, al)
+  end
 
-	# Code block
-	def md_codeblock(source, al=nil)
-		md_el(:code, [], {:raw_code => source}, al)
-	end
+  # Code block
+  def md_codeblock(source, al=nil)
+    md_el(:code, [], {:raw_code => source}, al)
+  end
 
-	def md_quote(children, al=nil)
-		md_el(:quote, children, {}, al)
-	end
+  def md_quote(children, al=nil)
+    md_el(:quote, children, {}, al)
+  end
 
-	def md_li(children, want_my_par, al=nil)
-		md_el(:li, children, {:want_my_paragraph=>want_my_par}, al)
-	end
+  def md_li(children, want_my_par, al=nil)
+    md_el(:li, children, {:want_my_paragraph=>want_my_par}, al)
+  end
 
-	def md_footnote(footnote_id, children, al=nil)
-		md_el(:footnote, children, {:footnote_id=>footnote_id}, al)
-	end
+  def md_footnote(footnote_id, children, al=nil)
+    md_el(:footnote, children, {:footnote_id=>footnote_id}, al)
+  end
 
-	def md_abbr_def(abbr, text, al=nil)
-		md_el(:abbr_def, [], {:abbr=>abbr, :text=>text}, al)
-	end
+  def md_abbr_def(abbr, text, al=nil)
+    md_el(:abbr_def, [], {:abbr=>abbr, :text=>text}, al)
+  end
 
-	def md_abbr(abbr, title)
-		md_el(:abbr, [abbr], {:title=>title})
-	end
+  def md_abbr(abbr, title)
+    md_el(:abbr, [abbr], {:title=>title})
+  end
 
-	def md_html(raw_html, al=nil)
-		e = md_el(:raw_html, [], {:raw_html=>raw_html})
-		begin
-			# remove newlines and whitespace at begin
-			# end end of string, or else REXML gets confused
-			raw_html = raw_html.gsub(/\A\s*</,'<').
-			                    gsub(/>[\s\n]*\Z/,'>')
+  def md_html(raw_html, al=nil)
+    e = md_el(:raw_html, [], {:raw_html=>raw_html})
+    begin
+      # remove newlines and whitespace at begin
+      # end end of string, or else REXML gets confused
+      raw_html = raw_html.gsub(/\A\s*</,'<').
+                          gsub(/>[\s\n]*\Z/,'>')
 
-			raw_html = "<marukuwrap>#{raw_html}</marukuwrap>"
-			e.instance_variable_set :@parsed_html,
-			 	REXML::Document.new(raw_html)
-		rescue   	REXML::ParseException => ex
-			e.instance_variable_set :@parsed_html, nil
-			maruku_recover "REXML cannot parse this block of HTML/XML:\n"+
-			add_tabs(raw_html,1,'|') + "\n"+ex.inspect
+      raw_html = "<marukuwrap>#{raw_html}</marukuwrap>"
+      e.instance_variable_set :@parsed_html,
+         REXML::Document.new(raw_html)
+    rescue   	REXML::ParseException => ex
+      e.instance_variable_set :@parsed_html, nil
+      maruku_recover "REXML cannot parse this block of HTML/XML:\n"+
+      add_tabs(raw_html,1,'|') + "\n"+ex.inspect
 #			"  #{raw_html.inspect}\n\n"+ex.inspect
-		end
-		e
-	end
+    end
+    e
+  end
 
-	def md_link(children, ref_id, al=nil)
+  def md_link(children, ref_id, al=nil)
     #puts "LINK>>> '#{ref_id}'"
-		md_el(:link, children, {:ref_id=>ref_id.downcase}, al)
-	end
+    md_el(:link, children, {:ref_id=>ref_id.downcase}, al)
+  end
 
-	def md_im_link(children, url, title=nil, al=nil)
-	  #puts "IM_LINK>>> '#{url}'"
-		md_el(:im_link, children, {:url=>url,:title=>title}, al)
-	end
+  def md_im_link(children, url, title=nil, al=nil)
+    #puts "IM_LINK>>> '#{url}'"
+    md_el(:im_link, children, {:url=>url,:title=>title}, al)
+  end
 
-	def md_image(children, ref_id, al=nil)
-		md_el(:image, children, {:ref_id=>ref_id}, al)
-	end
+  def md_image(children, ref_id, al=nil)
+    md_el(:image, children, {:ref_id=>ref_id}, al)
+  end
 
-	def md_im_image(children, url, title=nil, al=nil)
-		md_el(:im_image, children, {:url=>url,:title=>title},al)
-	end
+  def md_im_image(children, url, title=nil, al=nil)
+    md_el(:im_image, children, {:url=>url,:title=>title},al)
+  end
 
-	def md_em(children, al=nil)
-		md_el(:emphasis, [children].flatten, {}, al)
-	end
+  def md_em(children, al=nil)
+    md_el(:emphasis, [children].flatten, {}, al)
+  end
 
-	def md_br()
-		md_el(:linebreak, [], {}, nil)
-	end
+  def md_br()
+    md_el(:linebreak, [], {}, nil)
+  end
 
-	def md_hrule()
-		md_el(:hrule, [], {}, nil)
-	end
+  def md_hrule()
+    md_el(:hrule, [], {}, nil)
+  end
 
-	def md_strong(children, al=nil)
-		md_el(:strong, [children].flatten, {}, al)
-	end
+  def md_strong(children, al=nil)
+    md_el(:strong, [children].flatten, {}, al)
+  end
 
-	def md_emstrong(children, al=nil)
-		md_strong(md_em(children), al)
-	end
+  def md_emstrong(children, al=nil)
+    md_strong(md_em(children), al)
+  end
 
-	# <http://www.example.com/>
-	def md_url(url, al=nil)
-	  #puts "URL>>> '#{url}'"
-		md_el(:immediate_link, [], {:url=>url}, al)
-	end
+  # <http://www.example.com/>
+  def md_url(url, al=nil)
+    #puts "URL>>> '#{url}'"
+    md_el(:immediate_link, [], {:url=>url}, al)
+  end
 
-	# <andrea@rubyforge.org>
-	# <mailto:andrea@rubyforge.org>
-	def md_email(email, al=nil)
-		md_el(:email_address, [], {:email=>email}, al)
-	end
+  # <andrea@rubyforge.org>
+  # <mailto:andrea@rubyforge.org>
+  def md_email(email, al=nil)
+    md_el(:email_address, [], {:email=>email}, al)
+  end
 
-	def md_entity(entity_name, al=nil)
-		md_el(:entity, [], {:entity_name=>entity_name}, al)
-	end
+  def md_entity(entity_name, al=nil)
+    md_el(:entity, [], {:entity_name=>entity_name}, al)
+  end
 
-	# Markdown extra
-	def md_foot_ref(ref_id, al=nil)
-		md_el(:footnote_reference, [], {:footnote_id=>ref_id}, al)
-	end
+  # Markdown extra
+  def md_foot_ref(ref_id, al=nil)
+    md_el(:footnote_reference, [], {:footnote_id=>ref_id}, al)
+  end
 
-	def md_par(children, al=nil, line_no = -1)
-		#md_el(:paragraph, children, meta={}, al)
-		md_el(:paragraph, children, {:line_no => line_no}, al)
-	end
+  def md_par(children, al=nil, line_no = -1)
+    #md_el(:paragraph, children, meta={}, al)
+    md_el(:paragraph, children, {:line_no => line_no}, al)
+  end
 
-	# [1]: http://url [properties]
-	def md_ref_def(ref_id, url, title=nil, meta={}, al=nil)
-		meta[:url] = url
-		meta[:ref_id] = ref_id
-		meta[:title] = title if title
-		md_el(:ref_definition, [], meta, al)
-	end
+  # [1]: http://url [properties]
+  def md_ref_def(ref_id, url, title=nil, meta={}, al=nil)
+    meta[:url] = url
+    meta[:ref_id] = ref_id
+    meta[:title] = title if title
+    md_el(:ref_definition, [], meta, al)
+  end
 
-	# inline attribute list
-	def md_ial(al)
-		al = Maruku::AttributeList.new(al) if
-			not al.kind_of?Maruku::AttributeList
-		md_el(:ial, [], {:ial=>al})
-	end
+  # inline attribute list
+  def md_ial(al)
+    al = Maruku::AttributeList.new(al) if
+      not al.kind_of?Maruku::AttributeList
+    md_el(:ial, [], {:ial=>al})
+  end
 
-	# Attribute list definition
-	def md_ald(id, al)
-		md_el(:ald, [], {:ald_id=>id,:ald=>al})
-	end
+  # Attribute list definition
+  def md_ald(id, al)
+    md_el(:ald, [], {:ald_id=>id,:ald=>al})
+  end
 
-	# Server directive <?target code... ?>
-	def md_xml_instr(target, code)
-		md_el(:xml_instr, [], {:target=>target, :code=>code})
-	end
+  # Server directive <?target code... ?>
+  def md_xml_instr(target, code)
+    md_el(:xml_instr, [], {:target=>target, :code=>code})
+  end
 
 end
 end
@@ -195,62 +195,62 @@ end
 module MaRuKu
 
 class MDElement
-	# outputs abbreviated form  (this should be eval()uable to get the document)
-	def inspect2
-		s =
-		case @node_type
-		when :paragraph
-			"md_par(%s)" % children_inspect
-		when :footnote_reference
-			"md_foot_ref(%s)" % self.footnote_id.inspect
-		when :entity
-			"md_entity(%s)" % self.entity_name.inspect
-		when :email_address
-			"md_email(%s)" % self.email.inspect
-		when :inline_code
-			"md_code(%s)" % self.raw_code.inspect
-		when :raw_html
-			"md_html(%s)" % self.raw_html.inspect
-		when :emphasis
-			"md_em(%s)" % children_inspect
-		when :strong
-			"md_strong(%s)" % children_inspect
-		when :immediate_link
-			"md_url(%s)" % self.url.inspect
-		when :image
-			"md_image(%s, %s)" % [
-				children_inspect,
-				self.ref_id.inspect]
-		when :im_image
-			"md_im_image(%s, %s, %s)" % [
-				children_inspect,
-				self.url.inspect,
-				self.title.inspect]
-		when :link
-				"md_link(%s,%s)" % [
-					children_inspect, self.ref_id.inspect]
-		when :im_link
-				"md_im_link(%s, %s, %s)" % [
-					children_inspect,
-					self.url.inspect,
-					self.title.inspect,
-				]
-		when :ref_definition
-			"md_ref_def(%s, %s, %s)" % [
-					self.ref_id.inspect,
-					self.url.inspect,
-					self.title.inspect
-				]
-		when :ial
-			"md_ial(%s)" % self.ial.inspect
-		else
-			return nil
-		end
-		if @al and not @al.empty? then
-			s = s.chop + ", #{@al.inspect})"
-		end
-		s
-	end
+  # outputs abbreviated form  (this should be eval()uable to get the document)
+  def inspect2
+    s =
+    case @node_type
+    when :paragraph
+      "md_par(%s)" % children_inspect
+    when :footnote_reference
+      "md_foot_ref(%s)" % self.footnote_id.inspect
+    when :entity
+      "md_entity(%s)" % self.entity_name.inspect
+    when :email_address
+      "md_email(%s)" % self.email.inspect
+    when :inline_code
+      "md_code(%s)" % self.raw_code.inspect
+    when :raw_html
+      "md_html(%s)" % self.raw_html.inspect
+    when :emphasis
+      "md_em(%s)" % children_inspect
+    when :strong
+      "md_strong(%s)" % children_inspect
+    when :immediate_link
+      "md_url(%s)" % self.url.inspect
+    when :image
+      "md_image(%s, %s)" % [
+        children_inspect,
+        self.ref_id.inspect]
+    when :im_image
+      "md_im_image(%s, %s, %s)" % [
+        children_inspect,
+        self.url.inspect,
+        self.title.inspect]
+    when :link
+        "md_link(%s,%s)" % [
+          children_inspect, self.ref_id.inspect]
+    when :im_link
+        "md_im_link(%s, %s, %s)" % [
+          children_inspect,
+          self.url.inspect,
+          self.title.inspect,
+        ]
+    when :ref_definition
+      "md_ref_def(%s, %s, %s)" % [
+          self.ref_id.inspect,
+          self.url.inspect,
+          self.title.inspect
+        ]
+    when :ial
+      "md_ial(%s)" % self.ial.inspect
+    else
+      return nil
+    end
+    if @al and not @al.empty? then
+      s = s.chop + ", #{@al.inspect})"
+    end
+    s
+  end
 
 end
 
--- lib/omf-web/widget/text/text_widget.rb
+++ lib/omf-web/widget/text/text_widget.rb
@@ -4,49 +4,49 @@ require 'omf-web/content/repository'
 
 module OMF::Web::Widget
 
-  # Supports widgets which displays text with 
+  # Supports widgets which displays text with
   # potentially other widgets embedded.
   #
   class TextWidget < AbstractWidget
-    
+
     def self.create_text_widget(type, wdescr)
       return self.new(wdescr)
     end
-    
+
     def initialize(opts)
       opts = opts.dup # not sure why we may need to this. Is this hash used anywhere else?
-      super opts      
+      super opts
 
       unless (content_descr = opts[:content])
         raise "Missing 'content' option in '#{opts.inspect}'"
-      end      
+      end
       # if content_descr.is_a? OMF::Web::ContentProxy
         # self.content_proxy = content_descr
-      # else    
+      # else
         # #self.content_proxy = OMF::Web::ContentRepository[opts].load(content_descr)
         # self.content_proxy = OMF::Web::ContentRepository.create_content_proxy_for(content_descr, opts)
       # end
       self.content_proxy = OMF::Web::ContentRepository.create_content_proxy_for(content_descr, opts)
     end
-    
+
     def content_proxy=(content_proxy)
       @content_proxy = content_proxy
     end
-        
+
     def content()
-      update()      
+      update()
       OMF::Web::Theme.require 'text_renderer'
       @opts[:title] = @content.attributes[:title] || opts[:title]
       OMF::Web::Theme::TextRenderer.new(self, @content, @opts)
     end
-    
+
     def update()
       # Could avoid doing the next three steps every time if we would know if the
       # content in content_proxy has changed.
-      @content = OMF::Web::Widget::Text::Maruku.format_content(@content_proxy)
+      @content = OMF::Web::Widget::Text::Maruku.format_content_proxy(@content_proxy)
       @widgets = @content.attributes[:widgets] || []
     end
-    
+
 
     def content_url
       @content_proxy.content_url
@@ -55,7 +55,7 @@ module OMF::Web::Widget
     def content_id
       @content_proxy.content_id
     end
-    
+
     def mime_type
       'text/html'
     end
