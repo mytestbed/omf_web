fef3b0d467886e791514074d101da4beca57be33
Max Ott <max.ott@nicta.com.au>
2014-03-31 11:49:36 +1100

    whitespace

--- lib/omf-web/content/content_proxy.rb
+++ lib/omf-web/content/content_proxy.rb
@@ -4,19 +4,19 @@ require 'omf_base/lobject'
 require 'omf_web'
 
 module OMF::Web
-        
-  # This object maintains synchronization between a JS DataSource object 
+
+  # This object maintains synchronization between a JS DataSource object
   # in a web browser and the corresponding +OmlTable+ in this server.
   #
   #
   class ContentProxy < OMF::Base::LObject
-    
+
     @@proxies = {}
 
     def self.[](url)
       @@proxies[url.to_s]
     end
-    
+
     def self.create(content_descr, repo)
       unless key = content_descr[:url_key]
         raise "Missing ':url_key' in content descriptor '#{content_descr.inspect}'"
@@ -27,21 +27,21 @@ module OMF::Web
       debug "Create content proxy for '#{key}' (#{content_descr.inspect})"
       @@proxies[key] = self.new(content_descr, repo)
     end
-    
-    attr_reader :content_descriptor, :content_url, :content_id, :name, :mime_type
-    
-    
+
+    attr_reader :content_descriptor, :content_url, :content_id, :name, :mime_type, :repository
+
+
     def on_get(req)
       c = content()
       [c.to_s, "text"]
     end
-    
+
     def on_post(req)
       data = req.POST
       write(data['content'], data['message'])
       [true.to_json, "text/json"]
     end
-    
+
     def write(content, message = "")
       if content != @content
         debug "Updating '#{@content_descriptor.inspect}'"
@@ -49,15 +49,15 @@ module OMF::Web
         @repository.write(@content_descriptor, content, message)
       end
     end
-    
+
     def content()
       unless @content
         @content = @repository.read(@content_descriptor)
       end
       @content
     end
-    alias :read :content 
-    
+    alias :read :content
+
     # Return a new proxy for a url relative to this one
     def create_proxy_for_url(url)
       unless url.match ':'
@@ -72,21 +72,21 @@ module OMF::Web
     end
 
     private
-    
+
     def initialize(content_descriptor, repository)
       @content_descriptor = content_descriptor
       @repository = repository
       #@path = File.join(repository.top_dir, content_handle) # requires 1.9 File.absolute_path(@content_handle, @repository.top_dir)
-      
+
       @content_id = content_descriptor[:url_key]
       @content_url = "/_content/#{@content_id}"  # That most likley should come from the content handler
-      
+
       @mime_type = @content_descriptor[:mime_type] ||= repository.mime_type_for_file(content_descriptor)
       @name = content_descriptor[:name]
 
       @@proxies[@content_id] = self
     end
-    
+
   end
-  
+
 end
