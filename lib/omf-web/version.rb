71a88c4bfe873efee93cee5f0b8359ef124f987c
Max Ott <max.ott@nicta.com.au>
2014-07-28 18:33:15 +1000

    Version bump

--- lib/omf-web/version.rb
+++ lib/omf-web/version.rb
@@ -1,7 +1,7 @@
 
 module OMF
   module Web
-    VERSION = '1.2.8'
+    VERSION = '1.2.9'
     # Used for finding the example directory
     TOP_DIR = File.dirname(File.dirname(File.dirname(__FILE__)))
   end
