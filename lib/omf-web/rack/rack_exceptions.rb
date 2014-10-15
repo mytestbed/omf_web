bccac718a3afb6adcd86fe1645f0dc8c48661e01
Max Ott <max.ott@nicta.com.au>
2014-10-15 14:11:03 +1100

    Added new exceptions and made them easier to use - almost identical to omf_sfa

--- lib/omf-web/rack/rack_exceptions.rb
+++ lib/omf-web/rack/rack_exceptions.rb
@@ -1,7 +1,37 @@
 
 module OMF::Web::Rack
+
+  class RackException < Exception
+    attr_reader :reply
+
+    def initialize(err_code, reason)
+      super reason
+      body = {:exception => {
+        :code => err_code,
+        :reason => reason
+      }}
+      @reply = [err_code, {"Content-Type" => 'text/json'}, body.to_json]
+    end
+
+  end
+
+  class UnknownResourceException < RackException
+    def initialize(reason)
+      super 404, reason
+    end
+  end
+
+  class MissingResourceException < RackException
+    def initialize(reason)
+      super 404, reason
+    end
+  end
       
-  class MissingArgumentException < Exception; end
+  class MissingArgumentException < RackException
+    def initialize(reason = '')
+      super 412, reason
+    end
+  end
   
   class RedirectException < Exception
     
